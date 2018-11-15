e84fba395fcc7f5efcc1eeb08eefa79d718d909f
JM Taylor <jmotaylor2@gmail.com>
2018-11-16 02:49:45 +0900

    set marked options for linebreaks

--- src/index.js
+++ src/index.js
@@ -6,16 +6,14 @@ class PreviewerParent extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			inputValue: "# A Header!\n## And a subheader\nThis is [a link](https://jmotaylor.com)\nAnd this is some `inline code`\nNow if you want a block code, then check this out:\n```You can pretend...\nThat this here\'s some code,\nYou know what I\'m saying?```\nIf you want some list item, then here you go...\n- **Cool**\n- **Beans** (bold text, wow!) \n\nNow we need some blockquotes:\n> I guess this is a blockquote, right?\n\nAnd last but not least, the image...\n![React Logo w/ Text](https://goo.gl/Umyytc)",
+			inputValue: "# A Header!\n## And a subheader\nThis is [a link](https://jmotaylor.com)\nAnd this is some `inline code`\nNow if you want a block code, then check this out:\n```You can pretend...\nThat this here's some code,\nYou know what I'm saying?```\nIf you want some list item, then here you go...\n- **Cool**\n- **Beans** (bold text, wow!) \n\nNow we need some blockquotes:\n> I guess this is a blockquote, right?\n\nAnd last but not least, the image...\n![React Logo w/ Text](https://goo.gl/Umyytc)",
 		};
 		this.handleOnChange = this.handleOnChange.bind(this);
 	}
 
 	handleOnChange(event) {
 		this.setState({inputValue: event.target.value});
-		console.log(this.state.inputValue);
 	}
-
 	render() {
 		return (
 			<div id={"editor-preview"}>
