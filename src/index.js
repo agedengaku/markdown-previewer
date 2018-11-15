77193402b9acf89a8b31d226732874cce2fca4c4
JM Taylor <jmotaylor2@gmail.com>
2018-11-16 01:45:37 +0900

    complete user stories

--- src/index.js
+++ src/index.js
@@ -6,13 +6,14 @@ class PreviewerParent extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			inputValue: '*hihihi*',
+			inputValue: "# A Header!\n## And a subheader\nThis is [a link](https://jmotaylor.com)\nAnd this is some `inline code`\nNow if you want a block code, then check this out:\n```You can pretend...\nThat this here\'s some code,\nYou know what I\'m saying?```\nIf you want some list item, then here you go...\n- **Cool**\n- **Beans** (bold text, wow!) \n\nNow we need some blockquotes:\n> I guess this is a blockquote, right?\n\nAnd last but not least, the image...\n![React Logo w/ Text](https://goo.gl/Umyytc)",
 		};
 		this.handleOnChange = this.handleOnChange.bind(this);
 	}
 
 	handleOnChange(event) {
 		this.setState({inputValue: event.target.value});
+		console.log(this.state.inputValue);
 	}
 
 	render() {
@@ -31,14 +32,15 @@ class Editor extends React.Component {
 }
 
 class Preview extends React.Component {
-
 	constructor(props) {
 		super(props);
 		this.createMarkup = this.createMarkup.bind(this);
 	}
+
 	createMarkup() {
 		return {__html: window.marked(this.props.value)};
 	}
+
 	render() {
 		return (<div id={'preview'} dangerouslySetInnerHTML={this.createMarkup()}></div>);	
 	}
