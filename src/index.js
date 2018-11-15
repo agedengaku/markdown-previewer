27d514d612f587ce1f19e46e29906aa878a778e9
aburiengawa <jmotaylor2@gmail.com>
2018-11-15 13:05:30 +0900

    add textareas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class PreviewerParent extends React.Component {
+	render() {
+		return (
+			<div>
+				<Editor /><Preview />
+			</div>	
+		);
+	}
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Editor extends React.Component {
+	render() {
+		return (<textarea id={'editor'}></textarea>);	
+	}
+}
+
+class Preview extends React.Component {
+	render() {
+		return (<textarea id={'preview'} disabled={'true'}></textarea>);	
+	}
+}
+
+ReactDOM.render(<PreviewerParent />, document.getElementById('root'));
