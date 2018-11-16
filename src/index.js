db63671db68649ac8e0d9d2d484c73247d2d290d
JM Taylor <jmotaylor2@gmail.com>
2018-11-16 23:32:25 +0900

    Add editor focus and remove preview hover css, edit query, change default inputValue

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ class PreviewerParent extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			inputValue: "# I Love HEADERS!\n## And subheaders, too\nThis is [a link](https://jmotaylor.com).\nAnd this is some `inline code`.\nNow if you want a block o' code, then check this out:\n```\nYou can pretend that this here's some code,\nyou know what I'm saying?\n```\nList items? I got you covered!\n- **Cool**\n- **Beans** (bold text, wow!) \n\nNow we need some blockquotes:\n> I guess this is a blockquote, right?\n\nAnd last but not least, the image...\n\n![Of course it had to be a cat photo](https://i.kym-cdn.com/photos/images/newsfeed/000/012/445/lime-cat.jpg)\n\nOf course it had to be a cat photo.",
+			inputValue: "# I Love HEADERS!\n## And subheaders, too\nThis is [a link](https://jmotaylor.com).\nAnd this is some `inline code`.\nNow if you want a block o' code, then check this out:\n```\nI know, I know. These aren't really\nlines of code.\nBut you can pretend that they are.\nYou know what I'm saying?\n```\nList items? I got you covered!\n- **Cool**\n- **Beans** (bold text, wow!) \n\nNow we need some blockquotes:\n> I guess this is a blockquote, right?\n\nAnd last but not least, the image...\n\n![Of course it had to be a cat photo](https://i.kym-cdn.com/photos/images/newsfeed/000/012/445/lime-cat.jpg)\n\nOf course it had to be a cat photo.",
 		};
 		this.handleOnChange = this.handleOnChange.bind(this);
 	}
@@ -24,25 +24,15 @@ class PreviewerParent extends React.Component {
 	}
 }
 
-class Editor extends React.Component {
-	render() {
-		return (<textarea onChange={this.props.onChange} id={'editor'} value={this.props.value}></textarea>);	
-	}
+function Editor(props) {
+	return (<textarea onChange={props.onChange} id={'editor'} value={props.value}></textarea>);	
 }
 
-class Preview extends React.Component {
-	constructor(props) {
-		super(props);
-		this.createMarkup = this.createMarkup.bind(this);
+function Preview(props) {
+	function createMarkup() {
+		return {__html: window.marked(props.value)};
 	}
-
-	createMarkup() {
-		return {__html: window.marked(this.props.value)};
-	}
-
-	render() {
-		return (<div id={'preview'} dangerouslySetInnerHTML={this.createMarkup()}></div>);	
-	}
+	return (<div id={'preview'} dangerouslySetInnerHTML={createMarkup()}></div>)
 }
 
 ReactDOM.render(<PreviewerParent />, document.getElementById('root'));
