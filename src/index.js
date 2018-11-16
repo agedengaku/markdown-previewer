f09d750c64b39e0d86f6b2a282de73d2bb2eb7f1
aburiengawa <jmotaylor2@gmail.com>
2018-11-16 15:40:51 +0900

    fix css styles and edit editor/preview content

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './queries.css';
 
 class PreviewerParent extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			inputValue: "# A Header!\n## And a subheader\nThis is [a link](https://jmotaylor.com)\nAnd this is some `inline code`\nNow if you want a block code, then check this out:\n```You can pretend...\nThat this here's some code,\nYou know what I'm saying?```\nIf you want some list item, then here you go...\n- **Cool**\n- **Beans** (bold text, wow!) \n\nNow we need some blockquotes:\n> I guess this is a blockquote, right?\n\nAnd last but not least, the image...\n![React Logo w/ Text](https://goo.gl/Umyytc)",
+			inputValue: "# I Love HEADERS!\n## And subheaders, too\nThis is [a link](https://jmotaylor.com).\nAnd this is some `inline code`.\nNow if you want a block o' code, then check this out:\n```\nYou can pretend that this here's some code,\nyou know what I'm saying?\n```\nList items? I got you covered!\n- **Cool**\n- **Beans** (bold text, wow!) \n\nNow we need some blockquotes:\n> I guess this is a blockquote, right?\n\nAnd last but not least, the image...\n\n![Of course it had to be a cat photo](https://i.kym-cdn.com/photos/images/newsfeed/000/012/445/lime-cat.jpg)\n\nOf course it had to be a cat photo.",
 		};
 		this.handleOnChange = this.handleOnChange.bind(this);
 	}
